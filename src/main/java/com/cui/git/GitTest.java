2c38c514b94be4d5bdd9e262d5f4a798d1a06c05
nago <cuifuhua0082@163.com>
2022-04-24 14:43:16 +0800

    second commit

--- src/main/java/com/cui/git/GitTest.java
+++ src/main/java/com/cui/git/GitTest.java
@@ -4,5 +4,6 @@ package com.cui.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("i love you");
     }
 }
