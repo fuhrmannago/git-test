7b2393f19efb0dbc01e27006ad4444a58f91424f
nago <cuifuhua0082@163.com>
2022-04-30 00:21:56 +0800

    hot-fix commit1

--- src/main/java/com/cui/git/GitTest.java
+++ src/main/java/com/cui/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("i love you");
+        System.out.println("I don't care");
+        System.out.println("I don't care you");
     }
 }
